0200dce33f81b5b314bf958f08fe6f2ff00a186e
kaabo-art <kaarlemattila@gmail.com>
2022-08-10 11:09:21 +0300

    KaarleControlleriin TaikurinLisäys -metodin runko, input kesken. 3 Taikurikuvaa lisätty wwwroottiin

---
+++ TaikuriAppi/Controllers/KaarleController1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaikuriAppi.Controllers
+{
+    public class KaarleController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult TaikurinLisäys()
+        {
+            int taikuriid = input(taikuriid);               // nämä eivät vielä saa arvoja mistään !! 
+            string taiteilijanimi = input(taiteilijanimi);
+            string toimialat = input(toimialat);
+            string taidot = input(taidot);
+            string lokaatio = input(lokaatio);
+
+            Taikuri uusiTaikuri = new Taikuri()
+            {
+                TaikuriId = taikuriid,
+                Taiteilijanimi = taiteilijanimi,
+                Toimialat = toimialat,
+                Taidot = taidot,
+                Lokaatio = sijainti
+            };
+
+        }
+
+    }
+}
